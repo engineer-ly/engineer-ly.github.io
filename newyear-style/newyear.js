d854acec52ab6694b93fb907a872a0d8efc08318
Young Liu <535727301@qq.com>
2018-12-26 23:09:57 +0800

    e律师OA元旦彩蛋优化

--- newyear-style/newyear.js
+++ newyear-style/newyear.js
@@ -1,14 +1,25 @@
 $(function(){
+	$(".login_body").prepend("<img class='test-fireworks' src='img/fireworks.gif'>");
+
 	$(".login_body").prepend("<i class='img-left'></i>");
 	$(".login_body").prepend("<i class='img-right'></i>");
 	$(".login_body").prepend("<i class='img-bottom'></i>");
 	$(".login_body").prepend("<i class='img-top'></i>");
-	$(".login_body").prepend("<div class='start'>放烟花</div>");
+	$(".login_body").prepend("<i class='fireworks'></i>");
 
-	$(".login_body > .start").on("click",function(){
-		$(".login_body").addClass("fireworks");
-		setTimeout(function(){
-			$(".login_body").removeClass("fireworks");
-		},4000)
+	$(".login_body > .test-fireworks").load(function(){
+		$(".login_body").prepend("<div class='start'>看烟花</div>");
+		$(".login_body > .start").on("click",function(){
+			$(".login_body > .fireworks").addClass("show");
+			setTimeout(function(){
+				$(".login_body > .fireworks").removeClass("show");
+			},4000)
+		})
 	})
+			
+
+	
+	
+
+
 })
