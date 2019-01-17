9a03863ce9cd1a61dcb2eb5e77f126223d934f82
刘勇 <>
2019-01-17 14:41:33 +0800

    恢复

--- index/js/index.js
+++ index/js/index.js
@@ -1,7 +1,7 @@
-// var page = document.getElementById("page");
-// function randomNum(){
-// 	return parseInt(Math.random() * 10) + '';
-// }
-// setInterval(function(){
-// 	page.style.backgroundColor = "#" + randomNum() + randomNum() + "2e3c";
-// },4000)
+var page = document.getElementById("page");
+function randomNum(){
+	return parseInt(Math.random() * 10) + '';
+}
+setInterval(function(){
+	page.style.backgroundColor = "#" + randomNum() + randomNum() + "2e3c";
+},4000)
