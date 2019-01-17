6dbc8819d9c1628f75dad62fd8734f5871d54a6c
刘勇 <>
2019-01-17 14:32:38 +0800

    优化

--- index/js/index.js
+++ index/js/index.js
@@ -1,7 +1,14 @@
+// var page = document.getElementById("page");
+// function randomNum(){
+// 	return parseInt(Math.random() * 10) + '';
+// }
+// setInterval(function(){
+// 	page.style.backgroundColor = "#" + randomNum() + randomNum() + "2e3c";
+// },4000)
 var page = document.getElementById("page");
 function randomNum(){
 	return parseInt(Math.random() * 10) + '';
 }
 setInterval(function(){
-	page.style.backgroundColor = "#" + randomNum() + randomNum() + "2e3c";
-},4000)
+	page.style.backgroundColor = "#" + randomNum() + randomNum() + randomNum() + randomNum() + randomNum() + randomNum();
+},40)
