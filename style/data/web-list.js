7207b3a10cc13950c4390898c7c868df2e0a97dc
Young Liu <535727301@qq.com>
2019-01-20 20:52:55 +0800

    no message

--- style/data/web-list.js
+++ style/data/web-list.js
@@ -1,14 +1,70 @@
 var data = [
 {
-	"title": "每日灵感",
+	"title": "日常",
 	"content": [
 		{
 			"name": "Dribbble",
 			"logo": "style/img/Dribbble.svg",
-			"info": "界面、插画、平面等设计作品网站",
-			"label": ["英文","高质量","追波"],
-			"link": "https://dribbble.com/shots"
+			"label": ["英文","高质量","原创"],
+			"info": "你正在创作什么？",
+			"link": "https://dribbble.com"
+		},{
+			"name": "花瓣",
+			"logo": "style/img/huaban.png",
+			"label": ["中文","图片采集"],
+			"info": "为用户提供了一个简单的采集工具",
+			"link": "http://huaban.com"
+		},{
+			"name": "站酷",
+			"logo": "style/img/zhanku.png",
+			"label": ["中文","原创","社区"],
+			"info": '综合性"设计师社区"',
+			"link": "https://www.zcool.com.cn"
+		},{
+			"name": "oursketch",
+			"logo": "style/img/zhanku.png",
+			"label": ["中文","sketch","资源"],
+			"info": '少量优质sketch资源',
+			"link": "https://oursketch.com"
 		}
 	]
+},{
+	"title": "设计素材",
+	"content": [
+		{
+			"name": "Dribbble",
+			"logo": "style/img/Dribbble.svg",
+			"label": ["英文","高质量","原创"],
+			"info": "你正在创作什么？",
+			"link": "https://dribbble.com/shots"
+		},{
+			"name": "花瓣",
+			"logo": "style/img/huaban.png",
+			"label": ["中文","图片采集"],
+			"info": "为用户提供了一个简单的采集工具",
+			"link": "http://huaban.com"
+		},{
+			"name": "站酷",
+			"logo": "style/img/zhanku.png",
+			"label": ["中文","原创","社区"],
+			"info": '综合性"设计师社区"',
+			"link": "https://www.zcool.com.cn"
+		},{
+			"name": "站酷",
+			"logo": "style/img/zhanku.png",
+			"label": ["中文","原创","社区"],
+			"info": '综合性"设计师社区"',
+			"link": "https://www.zcool.com.cn"
+		}
+	]
+},{
+	"title": "摄影大图",
+	"content": ""
+},{
+	"title": "音效",
+	"content": ""
+},{
+	"title": "学习",
+	"content": ""
 }
 ]
