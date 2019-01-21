97a96cc8332567a4760e5a4f4fee870c791fc89c
刘勇 <>
2019-01-21 17:50:57 +0800

    no message

--- style/data/web-list.js
+++ style/data/web-list.js
@@ -32,29 +32,29 @@ var data = [
 	"title": "设计素材",
 	"content": [
 		{
-			"name": "Dribbble",
-			"logo": "style/img/Dribbble.svg",
-			"label": ["英文","高质量","原创"],
-			"info": "你正在创作什么？",
-			"link": "https://dribbble.com/shots"
+			"name": "pixabay",
+			"logo": "",
+			"label": ["免费","可商用","图片","视频"],
+			"info": "惊人的免费图片发现我们慷慨的社区分享的 1.6 百万张图片和视频。",
+			"link": "https://pixabay.com/"
 		},{
-			"name": "花瓣",
-			"logo": "style/img/huaban.png",
-			"label": ["中文","图片采集"],
-			"info": "为用户提供了一个简单的采集工具",
-			"link": "http://huaban.com"
+			"name": "55art",
+			"logo": "",
+			"label": ["素材","免费","灵感","专注"],
+			"info": "集UI、电商、配色、交流分享于一体的分类素材站",
+			"link": "http://www.55art.com/"
 		},{
-			"name": "站酷",
-			"logo": "style/img/zhanku.png",
-			"label": ["中文","原创","社区"],
-			"info": '综合性"设计师社区"',
-			"link": "https://www.zcool.com.cn"
+			"name":"飞特网",
+			"logo":"",
+			"label": ["平面","三维","影视后期"],
+			"info":"更懂分享！为分享创造价值",
+			"link":"http://www.fevte.com/"
 		},{
-			"name": "站酷",
-			"logo": "style/img/zhanku.png",
-			"label": ["中文","原创","社区"],
-			"info": '综合性"设计师社区"',
-			"link": "https://www.zcool.com.cn"
+			"name":"softufo",
+			"logo":"",
+			"label": ["软件下载"],
+			"info":"SoftUFO是国内最纯净安全的软件发源地，万千软件发烧友在此聚集，你的私人软件收藏夹！",
+			"link":"https://www.softufo.com/"
 		}
 	]
 },{
