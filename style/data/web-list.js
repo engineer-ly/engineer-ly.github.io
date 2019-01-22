28786e5c6f8aad9ff2b123f1e45723f4308478eb
Young Liu <535727301@qq.com>
2019-01-22 22:16:48 +0800

    no message

--- style/data/web-list.js
+++ style/data/web-list.js
@@ -59,12 +59,93 @@ var data = [
 	]
 },{
 	"title": "摄影大图",
-	"content": ""
+	"content": [
+		{
+			"name": "pixabay",
+			"logo": "",
+			"label": ["免费","可商用","图片","视频"],
+			"info": "惊人的免费图片发现我们慷慨的社区分享的 1.6 百万张图片和视频。",
+			"link": "https://pixabay.com/"
+		},{
+			"name": "55art",
+			"logo": "",
+			"label": ["素材","免费","灵感","专注"],
+			"info": "集UI、电商、配色、交流分享于一体的分类素材站",
+			"link": "http://www.55art.com/"
+		},{
+			"name":"飞特网",
+			"logo":"",
+			"label": ["平面","三维","影视后期"],
+			"info":"更懂分享！为分享创造价值",
+			"link":"http://www.fevte.com/"
+		},{
+			"name":"softufo",
+			"logo":"",
+			"label": ["软件下载"],
+			"info":"SoftUFO是国内最纯净安全的软件发源地，万千软件发烧友在此聚集，你的私人软件收藏夹！",
+			"link":"https://www.softufo.com/"
+		}
+	]
 },{
 	"title": "音效",
-	"content": ""
+	"content": [
+		{
+			"name": "pixabay",
+			"logo": "",
+			"label": ["免费","可商用","图片","视频"],
+			"info": "惊人的免费图片发现我们慷慨的社区分享的 1.6 百万张图片和视频。",
+			"link": "https://pixabay.com/"
+		},{
+			"name": "55art",
+			"logo": "",
+			"label": ["素材","免费","灵感","专注"],
+			"info": "集UI、电商、配色、交流分享于一体的分类素材站",
+			"link": "http://www.55art.com/"
+		},{
+			"name":"飞特网",
+			"logo":"",
+			"label": ["平面","三维","影视后期"],
+			"info":"更懂分享！为分享创造价值",
+			"link":"http://www.fevte.com/"
+		},{
+			"name":"softufo",
+			"logo":"",
+			"label": ["软件下载"],
+			"info":"SoftUFO是国内最纯净安全的软件发源地，万千软件发烧友在此聚集，你的私人软件收藏夹！",
+			"link":"https://www.softufo.com/"
+		}
+	]
 },{
 	"title": "学习",
-	"content": ""
+	"content": [
+		{
+			"name": "pixabay",
+			"logo": "",
+			"label": ["免费","可商用","图片","视频"],
+			"info": "惊人的免费图片发现我们慷慨的社区分享的 1.6 百万张图片和视频。",
+			"link": "https://pixabay.com/"
+		},{
+			"name": "55art",
+			"logo": "",
+			"label": ["素材","免费","灵感","专注"],
+			"info": "集UI、电商、配色、交流分享于一体的分类素材站",
+			"link": "http://www.55art.com/"
+		},{
+			"name":"飞特网",
+			"logo":"",
+			"label": ["平面","三维","影视后期"],
+			"info":"更懂分享！为分享创造价值",
+			"link":"http://www.fevte.com/"
+		},{
+			"name":"softufo",
+			"logo":"",
+			"label": ["软件下载"],
+			"info":"SoftUFO是国内最纯净安全的软件发源地，万千软件发烧友在此聚集，你的私人软件收藏夹！",
+			"link":"https://www.softufo.com/"
+		}
+	]
+},{
+	"title": "小帖士",
+	"content": []
 }
 ]
