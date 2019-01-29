1cf50d0e23180212e9a6fb48a7e6ebd3263bedb9
刘勇 <>
2019-01-29 17:33:57 +0800

    no message

--- yunfawu/Content/js/main.js
+++ yunfawu/Content/js/main.js
@@ -7,11 +7,16 @@ var navFn = {
 			$(it).addClass("on");
 		}else{
 			$("header .menu > a:eq(" + it + ")").addClass("on");
+			var w = $("header .menu").width();
+			var itw = $("header .menu > a:eq(" + it + ")").width();
+			var s = $("header .menu").scrollLeft();
+			var l = $("header .menu > a:eq(" + it + ")").position().left;
+			$("header .menu").scrollLeft(l+s-(w/2)+(itw/2));
 		}
 	}
 }
 $(function(){
-	// 菜单事件绑定
+	// 主菜单事件绑定
 	$("header .menu > a").click(function(){
 		var index = $(this).index();
 		navFn.on(this);
@@ -20,9 +25,10 @@ $(function(){
 		},300)
 		return false;
 	});
-	// 页面事件绑定
+	// 主菜单页面事件绑定
 	var navArr = new Array();
-	(function(){
+	function setNavScroll(){
+		navArr = new Array();
 		for(var i = 1; ;i ++ ){
 			if($("#point" + i).length){
 				navArr.push($("#point" + i).offset().top - 41);
@@ -30,11 +36,12 @@ $(function(){
 				break;
 			}
 		}
-	})();
+	};
 	var windowTimer;
 	$(window).scroll(function(){
 		clearTimeout(windowTimer);
 		windowTimer = setTimeout(function(){
+			setNavScroll();
 			var num = $(window).scrollTop();
 			var x = 0;
 			for (var i = 0; i <= navArr.length; i++) {
@@ -48,35 +55,71 @@ $(function(){
 })
 
 
+// 法律服务-菜单选中与页面滚动绑定
+var snavFn = {
+	on: function(it){
+		$(".law-list > .left > a").removeClass("on");
+		if(typeof(it) == "object"){
+			$(it).addClass("on");
+		}else{
+			$(".law-list > .left > a:eq(" + it + ")").addClass("on");
+		}
+	}
+}
 $(function(){
+	$(".law-list > .left > a").click(function(){
+		var index = $(this).index();
+		snavFn.on(this);
+		$("html,body").animate({
+			scrollTop:$("#s-point" + (index + 1)).offset().top - 60
+		},300)
+		return false;
+	});
+	// 法律服务子菜单页面事件绑定
+	var snavArr = new Array();
+	function setSnavScroll(){
+		snavArr = new Array();
+		for(var i = 1; ;i ++ ){
+			if($("#s-point" + i).length){
+				snavArr.push($("#s-point" + i).offset().top - 61);
+			}else{
+				break;
+			}
+		}
+	};
+	var s_windowTimer;
+	$(window).scroll(function(){
+		clearTimeout(s_windowTimer);
+		s_windowTimer = setTimeout(function(){
+			setSnavScroll();
+			var num = $(window).scrollTop();
+			var x = 0;
+			for (var i = 0; i <= snavArr.length; i++) {
+				if(num >= snavArr[i]){
+					x = i;
+				}
+			}
+			snavFn.on(x);
+		}, 50)
+	})
+})
+
+
+$(function(){
+	
 	// 页面滚动时
 	$(window).scroll(function(){
-		if($(this).scrollTop() > 200){
+		if($(this).scrollTop() > 100){
 			$("header").addClass("down");
 		}else{
 			$("header").removeClass("down");
 		}
-		
+		if($(this).scrollTop() > 240){
+			$(".law-list > .left").addClass('fixed');
+		}else{
+			$(".law-list > .left").removeClass('fixed');
+		}
 	})
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 })
